8ec4723d3775ae90492fdcdd17ae8d101fc00fc7
Todd Greco <mrballistic@gmail.com>
2026-03-15 10:54:55 -0700

    fix: convert jest.config.ts to jest.config.js to remove ts-node requirement

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
-// pi-birthday-finder/jest.config.ts
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
+const nextJest = require('next/jest.js')
 
 const createJestConfig = nextJest({ dir: './' })
 
-const config: Config = {
+const config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
@@ -13,4 +11,4 @@ const config: Config = {
   testMatch: ['**/__tests__/**/*.test.{ts,tsx}'],
 }
 
-export default createJestConfig(config)
+module.exports = createJestConfig(config)
